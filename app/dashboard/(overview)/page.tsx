c9a61111ff1ef5e3b951b5274e46e30a82acfe28
zengks <zengks@outlook.com>
2025-01-07 11:54:54 -0800

    end of chapter 16

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,20 +2,19 @@ import CardWrapper from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchCardData } from "../../lib/data";
+// import { fetchCardData } from "../../lib/data";
 import { Suspense } from "react";
 import {
  RevenueChartSkeleton,
  LatestInvoicesSkeleton,
  CardsSkeleton,
 } from "@/app/ui/skeletons";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+ title: "Dashboard",
+};
 export default async function Page() {
- const {
-  numberOfInvoices,
-  numberOfCustomers,
-  totalPaidInvoices,
-  totalPendingInvoices,
- } = await fetchCardData();
  return (
   <main>
    <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import SideNav from "../ui/dashboard/sidenav";
+import { Metadata } from "next";
 
 export const experimental_ppr = true;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
  return (
   <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
